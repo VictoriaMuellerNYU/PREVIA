c1a47cc8645a2900e1b3b198c42977657391b3e7
Victoria Mueller <victoria.mueller@nyulangone.org>
2026-03-30 15:29:10 -0400

    Reorganize files

--- src/run_previa.sh
+++ src/run_previa.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=llm_predict_job
-#SBATCH --partition=a100_short
 #SBATCH --nodes=1			# 1 node -> max 4 GPUs, 2 nodes -> max 8 GPUs -> adapt that in the script
 #SBATCH --ntasks=1	
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
-#SBATCH --time=0-2:00:00
+#SBATCH --time=0-3:00:00
 #SBATCH --gres=gpu:a100:3                   	# uses 2 GPUs - 1 node is sufficient
 #SBATCH --output=log_files/logs/llm_predict_%j.log	
 #SBATCH --error=log_files/errors/llm_predict_%j.err 
@@ -15,5 +14,5 @@ eval "$(conda shell.bash hook)"
 conda activate llm_predict
 
 # Your actual job command(s) go here
-python /gpfs/data/schultebrauckslab/Users/muellv01/llm_ed/experiments/multi_agent_AIME/code/zero_shot_script.py
+python /gpfs/data/schultebrauckslab/Users/muellv01/PREVIA/src/code/previa_pipeline.py
 
